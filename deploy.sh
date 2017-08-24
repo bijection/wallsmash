ed1679670a7291c866adfefd5832ecc38f068ed0
guillermo webster <guillermo@cdbzb.com>
2017-08-24 11:33:16 -0700

    add CNAME to deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,14 @@
 rm -rf dist
 yarn build
 cp index.html dist
+
 echo "CACHE MANIFEST" > dist/main.appcache
 echo '#' `date +%Y-%m-%d:%H:%M:%S` >> dist/main.appcache
 echo index.html >> dist/main.appcache
 echo bundle.js >> dist/main.appcache
 echo NETWORK: >> dist/main.appcache
 echo '*' >> dist/main.appcache
+
+echo wallsmash.com > dist/CNAME
+
 yarn upload
